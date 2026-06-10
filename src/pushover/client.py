87669ccdd5fcc7d7fc0c430c8e9ac1d743e32e0a
Collin Meyer <collinmmeyer@gmail.com>
2026-06-09 23:45:52 -0400

    clean up optional json args to send_message

--- src/pushover/client.py
+++ src/pushover/client.py
@@ -5,13 +5,14 @@ from pushover.const import MESSAGES, V1_JSON_API, Priority
 from pushover.errors import RequestError
 from pushover.models import Response
 
+HTTP_JSON = dict[str, str | int | bool | None]
 
 class PushoverClient(httpx.Client):
     """Pushover API Client"""
 
     def __init__(self, token: str, base_url: str = V1_JSON_API) -> None:
         super().__init__(base_url=base_url)
-        self.req_args = {
+        self.req_args: HTTP_JSON = {
             "token": token
         }
 
@@ -31,10 +32,23 @@ class PushoverClient(httpx.Client):
         url_title: str | None = None,
     ) -> Response:
         """"""
-        json = self.req_args | {
+        optional = {
+            "title": title,
+            "device": device,
+            "html": int(html) if html is not None else None,
+            "priority": priority,
+            "sound": sound,
+            "timestamp": timestamp,
+            "ttl": ttl,
+            "url": url,
+            "url_title": url_title,
+        }
+        json: HTTP_JSON = self.req_args | {
             "user": user,
             "message": message,
+            **{key: value for key, value in optional.items() if value is not None},
         }
+
         req_resp = self.post(MESSAGES, json=json)
         if req_resp.status_code >= httpx.codes.BAD_REQUEST:
             raise RequestError(req_resp.text)
